4a67e97cf8055cb33ff2011a82455d1e1304a49c
rkotenko <robkotenko@gmail.com>
2015-06-01 15:20:18 -0400

    fetching, directives, etc
    This controller now has a lot of stuff in it.  Too much.  And it
    fetches from a mock json server api.  But the point was to see how
    $http works as well as some directives and stuff. So mission complete.
    db.json is the fake database.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
-// MODULE
+// None of the code here is well organized.  It is strictly being used
+// for learning.
+
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS - the array in the 2nd parameter is needed so the function params can be
 // mapped properly if the code is minified.
-angularApp.controller('mainController',  ['$scope', '$filter',
-function ($scope, $filter) {
+angularApp.controller('mainController',  ['$scope', '$filter', '$http',
+function ($scope, $filter, $http) {
   $scope.handle = '';
+  $scope.newRule = '';
+  $scope.rulesNextId = 0; // gonna grabbed the highest id on initial fetch
   $scope.lowercaseHandle = function() {
     return $filter('lowercase')($scope.handle);
-  }
+  };
+  
+  $scope.characters = 5;
+  
+  // get the rules in descending order by id so the nextId can be set properly
+  $http.get('http://localhost:5000/api?_sort=id&_order=DESC')
+      .success(function (result) {
+        $scope.rules = result;
+        $scope.rulesNextId = result[0].id + 1;
+      })
+      .error(function (error, status) {
+        console.log(data);
+      });
+  
+  // this uses a mock json server.
+  $scope.addRule = function () {
+    $http.post('http://localhost:5000/api', {id: $scope.rulesNextId, rulename: $scope.newRule});
+    $scope.rulesNextId++;
+    $scope.rules.push({rulename: $scope.newRule});
+    $scope.newRule = '';
+  };
+  
 }]);
 
