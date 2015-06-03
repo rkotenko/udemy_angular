50901e7eef55602cac8aeabcfa67345c7aa4a075
rkotenko <robkotenko@gmail.com>
2015-06-03 08:31:26 -0400

    adding in project settings so I can use elsewhere
    I am using Eclipse at work and WebStorm at home so I want to keep my
    project files for each so I can use github at a transfer method.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # udemy_angular
+
+Taking an angular course on Udemy.  It is unlikely anyone is going to grab this project and use it (it is just learning code after all), but if you do, you will want to delete the "." folders.  I have them there because I am using github to transfer between work and home for this and I am using different IDEs.
--- js/app.js
+++ js/app.js
@@ -1,38 +1,42 @@
-// None of the code here is well organized.  It is strictly being used
+// None of the code here is very well organized.  It is strictly being used
 // for learning.
 
-var angularApp = angular.module('angularApp', []);
+var angularApp = angular.module('angularApp', ['ngRoute']);
 
-// CONTROLLERS - the array in the 2nd parameter is needed so the function params can be
-// mapped properly if the code is minified.
-angularApp.controller('mainController',  ['$scope', '$filter', '$http',
-function ($scope, $filter, $http) {
-  $scope.handle = '';
-  $scope.newRule = '';
-  $scope.rulesNextId = 0; // gonna grabbed the highest id on initial fetch
-  $scope.lowercaseHandle = function() {
-    return $filter('lowercase')($scope.handle);
-  };
+angularApp.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
+    })
+    .when('/second/:num?', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    });
   
-  $scope.characters = 5;
-  
-  // get the rules in descending order by id so the nextId can be set properly
-  $http.get('http://localhost:5000/api?_sort=id&_order=DESC')
-      .success(function (result) {
-        $scope.rules = result;
-        $scope.rulesNextId = result[0].id + 1;
-      })
-      .error(function (error, status) {
-        console.log(data);
-      });
-  
-  // this uses a mock json server.
-  $scope.addRule = function () {
-    $http.post('http://localhost:5000/api', {id: $scope.rulesNextId, rulename: $scope.newRule});
-    $scope.rulesNextId++;
-    $scope.rules.push({rulename: $scope.newRule});
-    $scope.newRule = '';
-  };
-  
+});
+
+angularApp.controller('mainController',  ['$scope', '$log', 
+  function ($scope, $log) {
+    $scope.person = {
+        name: 'Rob K',
+        address: '555 Test st, Anytown'
+    };
 }]);
+
+angularApp.controller('secondController',  ['$scope', '$log', '$routeParams',
+  function ($scope, $log, $routeParams) {
+    
+   
+}]);
+
+angularApp.directive('searchResult', function() {
+  return {
+    templateUrl: 'directives/searchResults.html',
+    replace: true,
+    scope: {
+      person: '='
+    }
+  };
+});
 
